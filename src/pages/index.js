649a2765539445a60ba046a9d1ab849f2d47265c
Michael Nekrasov <mikrasov@gmail.com>
2020-06-27 11:02:38 -0700

    Make time update every second

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,17 @@ export default function(props) {
     // ----------------------
     const [time, setTime] = useState(0)
     useEffect(() => {
-        // get data from GitHub api
-        fetch(`http://worldtimeapi.org/api/timezone/America/Los_Angeles`)
-            .then(response => response.json()) // parse JSON from request
-            .then(resultData => {
-                setTime(resultData.datetime)
 
-            }) // set data for the number of stars
+        window.setInterval(function(){
+
+            fetch(`http://worldtimeapi.org/api/timezone/America/Los_Angeles`)
+                .then(response => response.json()) // parse JSON from request
+                .then(resultData => {
+                    setTime(resultData.datetime)
+
+                })
+
+        }, 1000);
     }, [])
 
     return <Layout>
