9054297f06f48179e0bf4847548831bc09e67e31
Michael Nekrasov <mikrasov@gmail.com>
2020-07-06 15:56:36 -0700

    Undo react-dom hot-load patch as it was causing issues.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,14 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-    if (stage.startsWith("develop")) {
-        actions.setWebpackConfig({
-            resolve: {
-                alias: {
-                    "react-dom": "@hot-loader/react-dom",
-                },
-            },
-        })
-    }
-}
