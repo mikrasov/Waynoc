e9678e58dac588e507766b3887dbd5e29f6af974
Michael Nekrasov <mikrasov@gmail.com>
2020-07-13 14:46:20 -0700

    Moved game control bar into its own component.
    Added a bit of styling to control bar

---
+++ src/components/controls/control_bar.js
@@ -0,0 +1,48 @@
+import React  from "react"
+import { Button, Col, Row} from "react-bootstrap"
+import {connect} from "react-redux"
+import {makeChoice, nextSeason} from "../../util/event_director"
+import ChangeName from "./change_name";
+
+import {graphql, useStaticQuery} from "gatsby";
+
+function GameControls({dispatch, events, player}) {
+
+    const data  = useStaticQuery( graphql`{
+        events: allMarkdownRemark(
+            sort: {
+              fields: [frontmatter___age]
+              order: ASC
+            }
+        ){
+            nodes {
+                id
+            }
+        }
+    }`)
+
+    const activeEvent = events.activeEvent
+
+    let nextSeasonControl = <>
+        <Col lg={9} sm={6}/>
+        <Col lg={3} sm={6}><Button type="button"  size="lg"  onClick={ ()=>{nextSeason(player, data.events.nodes, dispatch)} }>Next Season</Button></Col>
+    </>
+
+
+    if(activeEvent?.input === "name"){
+        nextSeasonControl = <ChangeName/>
+    }
+    else if(activeEvent?.choices?.length){
+        nextSeasonControl = activeEvent.choices.map((choice,index) => <Col lg={3} sm={6} key={index}>
+                <Button type="button"  size="lg"  onClick={ ()=>{makeChoice(activeEvent,index,dispatch)} } >{choice.label}</Button>
+            </Col>
+        )
+    }
+
+    return <div className="gamecontrols">
+        {activeEvent.prompt && <h3>{activeEvent.prompt}</h3>}
+
+        <Row>{nextSeasonControl}</Row>
+    </div>
+}
+export default connect(state => ({ events: state.events, player: state.player,}), null)(GameControls)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,26 @@
 import React  from "react"
 import { connect } from 'react-redux'
-import {Row, Col, Button} from "react-bootstrap"
-import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/layout"
-import ChangeName from "../components/controls/change_name"
-import {nextSeason, makeChoice} from "../util/event_director"
+import GameControls from "../components/controls/control_bar"
 
+function IndexPage({ events, dispatch} ) {
 
-function IndexPage({player, events, dispatch} ) {
-
-    const data  = useStaticQuery( graphql`{
-        events: allMarkdownRemark(
-            sort: {
-              fields: [frontmatter___age]
-              order: ASC
-            }
-        ){
-            nodes {
-                id
-            }
-        }
-    }`)
-
-    const currentEvent = events.activeEvent
-
-    let nextSeasonControl = <>
-        <Col lg={9} sm={6}/>
-        <Col lg={3} sm={6}><Button type="button"  size="lg"  onClick={ ()=>{nextSeason(player, data.events.nodes, dispatch)} }>Next Season</Button></Col>
-    </>
-
-
-    if(currentEvent?.input === "name"){
-        nextSeasonControl = <ChangeName/>
-    }
-    else if(currentEvent?.choices?.length){
-        nextSeasonControl = currentEvent.choices.map((choice,index) => <Col lg={3} sm={6} key={index}>
-            <Button type="button"  size="lg"  onClick={ ()=>{makeChoice(currentEvent,index,dispatch)} } >{choice.label}</Button>
-        </Col>
-        )
-    }
 
     return (
         <Layout active={"game"}>
 
-            <h1>{currentEvent.title}</h1>
-
-            { currentEvent.parts.map((event,index) => <div key={index}>{event}</div>)}
-
-            <div className="gamecontrols">
-                {currentEvent.prompt && <h3>{currentEvent.prompt}</h3>}
-
-                <Row>{nextSeasonControl}</Row>
+            <h1>{events.activeEvent.title}</h1>
+            <div className={"event-body"}>
+                { events.activeEvent.parts.map((event,index) => <div key={index}>{index>0?<hr/>:""}{event}</div>)}
             </div>
 
+            <GameControls/>
         </Layout>
     )
 }
 
 export default connect(state => ({
         events: state.events,
-        player: state.player,
     }), null)(IndexPage)
 
